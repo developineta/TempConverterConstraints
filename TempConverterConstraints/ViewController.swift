d4a2d1c831885a9cef927bf5ebea32c84774d4f4
developineta <77379211+developineta@users.noreply.github.com>
2021-11-16 18:41:56 +0200

    Functionalities of the application
    Connecting elements to the ViewController and addition of functions

--- TempConverterConstraints/ViewController.swift
+++ TempConverterConstraints/ViewController.swift
@@ -9,11 +9,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var inCelsius: UILabel!
+    @IBOutlet weak var sliderOfDegrees: UISlider!
+    @IBOutlet weak var convertedTemperature: UILabel!
+    @IBOutlet weak var tempSegmentControl: UISegmentedControl!
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        convertedTemperature.text = "32 ºF"
     }
 
-
+    @IBAction func sliderValueChanged(_ sender: Any) {
+        print("sliderOfDegrees:" , sliderOfDegrees.value)
+        updateTempLabelForSlider(value: sliderOfDegrees.value)
+    }
+    
+    @IBAction func segmentValueChanged(_ sender: Any) {
+        print("tempSlider:" , sliderOfDegrees.value)
+        updateTempLabelForSlider(value: sliderOfDegrees.value)
+    }
+    
+    func updateTempLabelForSlider(value: Float){
+        let celsiusTemp = Int(value)
+        
+        inCelsius.text = "\(celsiusTemp) ºC"
+        
+        var convertedTempString = ""
+        switch tempSegmentControl.selectedSegmentIndex {
+        case 0:
+            let fahreinheitTempString = String(format: "%.2F", convertTempFrom(celsius: celsiusTemp).fahrenheit)
+            convertedTempString = fahreinheitTempString + " ºF"
+        default:
+            let kelvinTempString = String(format: "%.2F", convertTempFrom(celsius: celsiusTemp).kelvin)
+            convertedTempString = kelvinTempString + " ºK"
+        }
+        convertedTemperature.text = convertedTempString
+    }
+    
+    func convertTempFrom(celsius: Int) -> (fahrenheit: Double, kelvin: Double){
+        let fahrenheit = (Double(celsius) * 9 / 5) + 32
+        let kelvin = Double(celsius) + 273.15
+        return (fahrenheit, kelvin)
+    }
 }
 
